4a6ae8fb21d24098a59094c1b41c341acb095c45
Dante4K18 <daniel.ombui@student.moringaschool.com>
2024-07-09 18:43:54 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; 
+console.log(multiply); 
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1; 
+
+console.log(random); 
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4; 
+
+console.log(mod); 
+
+const numbers = [11, 12, 13, 14, 16, 17, 20];
+const max = Math.max(...numbers); 
+
+console.log(max); 
+
